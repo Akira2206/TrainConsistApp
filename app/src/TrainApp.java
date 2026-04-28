c6d2a88f4b5725301241287b7b6019007065198d
Rakshita Chauhan <chauhanrakshita64@gmail.com>
2026-04-29 03:06:42 +0530

    Refactor TrainApp: Remove Bogie class and add performance comparison

--- app/src/TrainApp.java
+++ app/src/TrainApp.java
@@ -1,43 +1,23 @@
 import java.util.*;
-import java.util.function.Predicate;
-
-class Bogie {
-    String type;
-    int capacity;
-    String cargoType;
-    boolean isCylindrical;
-
-    Bogie(String type, int capacity) {
-        this.type = type;
-        this.capacity = capacity;
-    }
-
-    Bogie(String type, String cargoType, boolean isCylindrical) {
-        this.type = type;
-        this.cargoType = cargoType;
-        this.isCylindrical = isCylindrical;
-    }
-
-    public int getCapacity() { return capacity; }
-}
 
 public class TrainApp {
     public static void main(String[] args) {
-        List<Bogie> goodsTrain = Arrays.asList(
-            new Bogie("Wagon", "Coal", false),
-            new Bogie("Tanker", "Oil", true),
-            new Bogie("Tanker", "Gas", true)
-        );
+        List<Integer> capacities = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) capacities.add(i);
 
-        System.out.println("--- UC12: Safety Compliance Check ---");
-        Predicate<Bogie> safetyRule = b -> !b.isCylindrical || (b.cargoType.equals("Oil") || b.cargoType.equals("Gas"));
+        System.out.println("--- UC13: Performance Comparison ---");
 
-        boolean isSafe = goodsTrain.stream().allMatch(safetyRule);
-
-        if (isSafe) {
-            System.out.println("All goods bogies comply with safety regulations.");
-        } else {
-            System.out.println("Safety Alert: Hazardous cargo detected in improper container!");
+        long startLoop = System.nanoTime();
+        int sumLoop = 0;
+        for (int c : capacities) {
+            if (c % 2 == 0) sumLoop += c;
         }
+        long endLoop = System.nanoTime();
+        System.out.println("Loop Time: " + (endLoop - startLoop) + " ns");
+
+        long startStream = System.nanoTime();
+        long sumStream = capacities.stream().filter(c -> c % 2 == 0).mapToInt(Integer::intValue).sum();
+        long endStream = System.nanoTime();
+        System.out.println("Stream Time: " + (endStream - startStream) + " ns");
     }
 }
