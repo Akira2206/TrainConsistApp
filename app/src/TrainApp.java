243f29d7d94858521302ba5e67b22e07649d9851
Rakshita Chauhan <chauhanrakshita64@gmail.com>
2026-04-29 03:04:35 +0530

    Implement validation for Train ID and Cargo Codes

--- app/src/TrainApp.java
+++ app/src/TrainApp.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.regex.*;
 
 class Bogie {
     String type;
@@ -22,13 +23,25 @@ public class TrainApp {
         );
 
         System.out.println("--- UC10: Calculating Total Seating Capacity ---");
-
         int totalSeats = train.stream()
             .map(Bogie::getCapacity)
             .reduce(0, (sum, cap) -> sum + cap);
-
-
         System.out.println("Total Seating Capacity for the Train: " + totalSeats);
-        System.out.println("Ready for passenger boarding.");
+
+        System.out.println("\n--- UC11: Validate Train ID & Cargo Codes ---");
+        String trainID = "TRN-12345";
+        String cargoCode = "CRG99";
+
+        String trainRegex = "^TRN-\\d{5}$";
+        String cargoRegex = "^[A-Z]{3}\\d{2}$";
+
+        boolean isTrainValid = Pattern.matches(trainRegex, trainID);
+        boolean isCargoValid = Pattern.matches(cargoRegex, cargoCode);
+
+        if (isTrainValid && isCargoValid) {
+            System.out.println("Validation Successful: Train ID and Cargo Code are in correct format.");
+        } else {
+            System.out.println("Validation Failed: Check ID/Code formats.");
+        }
     }
 }
