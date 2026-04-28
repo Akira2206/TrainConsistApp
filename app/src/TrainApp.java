497da93f220a0bca56440e459d9c09e9f617efb4
Rakshita Chauhan <chauhanrakshita64@gmail.com>
2026-04-29 03:10:12 +0530

    Implement bubble sort for cargo capacities

--- app/src/TrainApp.java
+++ app/src/TrainApp.java
@@ -1,18 +1,20 @@
 public class TrainApp {
     public static void main(String[] args) {
-        System.out.println("--- UC15: Safe Cargo Assignment ---");
-        String cargo = "Chemicals";
-        boolean isSafeContainer = false;
+        int[] capacities = {72, 24, 56, 72, 18};
+        System.out.println("--- UC16: Bubble Sort Capacities ---");
 
-        try {
-            if (!isSafeContainer) {
-                throw new Exception("Incompatible cargo for this bogie type!");
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
             }
-            System.out.println("Cargo assigned: " + cargo);
-        } catch (Exception e) {
-            System.err.println("Assignment Failed: " + e.getMessage());
-        } finally {
-            System.out.println("Cleaning up assignment logs...");
         }
+        
+        System.out.print("Sorted Capacities: ");
+        for (int c : capacities) System.out.print(c + " ");
+        System.out.println();
     }
 }
